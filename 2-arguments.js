ed41796c5bd1386977e65ef8210153a303b09835
ganiyu lukman <lganiyu43@gmail.com>
2025-11-06 22:50:54 +0100

    task 2: arguments completed

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,8 @@
-const args = proccess.argv.slice(2);
-// take only real arguments
+const args = process.argv.slice(2);
 
-if (args: length === 0) {
-  console.log("No arguments");
-} else if (args: length === 1) {
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
   console.log("Argument found");
 } else {
   console.log("Arguments found");
