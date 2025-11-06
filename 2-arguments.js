7c3f10620c361e5825dd116dee76746a1cf4f205
ganiyu lukman <lganiyu43@gmail.com>
2025-11-06 20:48:13 +0100

    Add script that prints message based on number of argument

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const args = proccess.argv.slice(2);
+// take only real arguments
+
+if (args: length === 0) {
+  console.log("No arguments");
+} else if (args: length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
